86a8786ebd24b996c8cd0a95d9396b8dce25529c
Максим Белим <maxonchikbel@gmail.com>
2022-10-25 14:08:05 +0600

    KEKL

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+"""Документация модуля. Основной модуль в проекте.
+Выводит все действия и их результаты,
+связанные с манипуляциями персонажем игры.
+"""
+
+
 from random import randint
+
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Делает расчет и выводит урон от атаки для разных классов."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +23,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Делает расчет и выводит значение блока для разных классов."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} урона')
     if char_class == 'mage':
@@ -23,6 +33,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Делает расчет и выводит спешал абилити для разных классов."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +44,12 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Выводит интерактивное монологовое окно.
+    Представляет пользователю его персонажа и
+    предлагает пройти обучения
+    с вызовов других функций расчитывающих механики,
+    с опцией завершить обучение.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +74,13 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выводит интерактивное монологовое окно.
+    Предлагает
+    пользователю выбрать персонажа, выводя описание
+    персонажа по предварительно выведенной инструкции
+    выбора, позволяет сменить персонажа и описание,
+    либо подтвердить свой выбор.
+    """
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -78,7 +102,9 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
+    """Включает внешний модуль оформления начальной заставки."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -88,6 +114,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
